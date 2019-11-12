868a17d517321a6ae9bf956d698bf0c48100d386
manishgdev <manishgdev@gmail.com>
2019-11-12 12:59:53 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-show-hide-div
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-show-hide-div)
---
+++ src/app/button-toggle-overview-example.ts
@@ -0,0 +1,23 @@
+import {Component} from '@angular/core';
+
+/**
+ * @title Basic button-toggles
+ */
+@Component({
+  selector: 'button-toggle-overview-example',
+  templateUrl: 'button-toggle-overview-example.html',
+  styleUrls: ['button-toggle-overview-example.css'],
+})
+export class ButtonToggleOverviewExample {
+
+  public currState:boolean=false;
+
+  showHideBelowDiv() {
+    this.currState = !this.currState;
+  }
+}
+
+
+/**  Copyright 2019 Google LLC. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at http://angular.io/license */
